1c7b89500e49eb3ecd1e6ecdda462f3ed7859847
marianosandoval07-sy <mbenitezsandoval07@gmail.com>
2026-04-17 17:02:51 -0600

    se agrego el init a la clase

--- practica1.py
+++ practica1.py
@@ -1,2 +1,9 @@
 class Celular:
-    pass
+    def __init__(self,nombre,marca,modelo,almacenamiento,memoria,bateria):
+        self.__nombre = nombre
+        self.__marca = marca
+        self.__modelo = modelo
+        self.__almacenamiento = almacenamiento
+        self.__memoria = memoria
+        self.__bateria = bateria
+        
